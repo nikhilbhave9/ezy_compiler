9c5d1f5d05a2882da90927772810a7ed0c2e698f
Nikhil Bhave <nik.3731@gmail.com>
2021-12-09 21:22:27 +0530

    Added template parsing code

--- main.py
+++ main.py
@@ -96,7 +96,6 @@ t_NEQ       = r'\<\>'
 # ========== Regular expression rules with some action code ==========
 def t_STRING(t):
     r'\".*\"'
-    # r'"([^"\n]|(\\"))*"$'
     t.value = (t.value)[1:-1] # removing the double quotes
     return t
     
@@ -197,3 +196,62 @@ while True:
 
 
 
+# ======================= End of Lexical Analysis  ======================= 
+
+# ==========================================
+# Building the parser
+# ==========================================
+
+# Get the token map from the lexer.  
+# from calclex import tokens (UNCOMMENT THIS LINE ONLY IF YOUR LEXER IS IN A DIFFERENT FILE)
+
+# Get the token map from the lexer.  This is required.
+from calclex import tokens
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = p[1] + p[3]
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = p[1] - p[3]
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = p[1] * p[3]
+
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    p[0] = p[1] / p[3]
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    p[0] = p[1]
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+# Build the parser
+parser = yacc.yacc()
+
+while True:
+   try:
+       s = input('calc > ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print(result)
